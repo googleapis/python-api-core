96f8b170c7e1bbce5591f42698887a8ad227c050
Lidi Zheng <lidiz@google.com>
2020-04-17 15:30:19 -0700

    Add FakeStreamUnaryCall to ease mocking

--- google/api_core/grpc_helpers_async.py
+++ google/api_core/grpc_helpers_async.py
@@ -250,3 +250,24 @@ class FakeUnaryUnaryCall(_WrappedUnaryUnaryCall):
     def __await__(self):
         response = yield from self._future.__await__()
         return response
+
+
+class FakeStreamUnaryCall(_WrappedStreamUnaryCall):
+    """Fake implementation for stream-unary RPCs.
+
+    It is a dummy object for response message. Supply the intended response
+    upon the initialization, and the coroutine will return the exact response
+    message.
+    """
+
+    def __init__(self, response=object()):
+        self.response = response
+        self._future = asyncio.get_event_loop().create_future()
+        self._future.set_result(self.response)
+
+    def __await__(self):
+        response = yield from self._future.__await__()
+        return response
+
+    async def wait_for_connection(self):
+        pass
