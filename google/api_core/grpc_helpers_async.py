385a710496dac6db588fcca303881496a0a3eaba
Bu Sun Kim <busunkim@google.com>
2020-06-18 23:10:17 +0000

    docs: fix in async too

--- google/api_core/grpc_helpers_async.py
+++ google/api_core/grpc_helpers_async.py
@@ -228,7 +228,7 @@ def create_channel(target, credentials=None, scopes=None, ssl_credentials=None, 
         aio.Channel: The created channel.
 
     Raises:
-        ValueError: If both a credentials object and credentials_file are passed.
+        google.api_core.DuplicateCredentialArgs: If both a credentials object and credentials_file are passed.
     """
 
     composite_credentials = grpc_helpers._create_composite_credentials(
