ce74ebcac381f1273373cdc6e865aa24773f9586
Anthonios Partheniou <partheniou@google.com>
2022-05-26 20:15:58 +0000

    fix(deps): require googleapis-common-protos>=1.56.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ description = "Google API client core library"
 # 'Development Status :: 5 - Production/Stable'
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "googleapis-common-protos >= 1.52.0, < 2.0dev",
+    "googleapis-common-protos >= 1.56.2, < 2.0dev",
     "protobuf >= 3.12.0, <4.0.0dev",
     "google-auth >= 1.25.0, < 3.0dev",
     "requests >= 2.18.0, < 3.0.0dev",
