e0877ad28b46debd700b7fad4a682232b736bc8e
Victor Chudnovsky <vchudnov@google.com>
2022-11-07 17:55:40 -0800

    Fix import order

--- tests/unit/gapic/test_routing_header.py
+++ tests/unit/gapic/test_routing_header.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
-
 from enum import Enum
+
+import pytest
 
 try:
     import grpc  # noqa: F401
