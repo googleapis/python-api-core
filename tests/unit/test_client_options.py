7cb86e9e4a2a83ebf130749c69190f1a476324a9
Victor Chudnovsky <vchudnov@google.com>
2023-03-13 15:11:16 -0700

    fix syntax

--- tests/unit/test_client_options.py
+++ tests/unit/test_client_options.py
@@ -148,8 +148,8 @@ def test_repr():
 
     assert (
         (repr(options)
-         == "ClientOptions: {'api_endpoint': 'foo.googleapis.com', 'client_cert_source': None, 'client_encrypted_cert_source': None, 'api_key': None}"
+         == "ClientOptions: {'api_endpoint': 'foo.googleapis.com', 'client_cert_source': None, 'client_encrypted_cert_source': None, 'api_key': None}")
          or
          (repr(options)
-          == "ClientOptions: {'client_encrypted_cert_source': None, 'client_cert_source': None, 'api_endpoint': 'foo.googleapis.com', 'api_key': None}"))
+          == "ClientOptions: {'client_encrypted_cert_source': None, 'client_cert_source': None, 'api_endpoint': 'foo.googleapis.com', 'api_key': None}")
     )
