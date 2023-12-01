0f1145d9d94c7b9d5021a43328a555914380e2c0
Daniel Sanche <d.sanche14@gmail.com>
2023-11-30 17:24:57 -0800

    revised docstring

--- google/api_core/retry/retry_unary.py
+++ google/api_core/retry/retry_unary.py
@@ -97,7 +97,7 @@ def retry_target(
     higher-level retry helper :class:`Retry`.
 
     Args:
-        target(Callable[[], Any]): The function to call and retry. This must be a
+        target(Callable): The function to call and retry. This must be a
             nullary function - apply arguments with `functools.partial`.
         predicate (Callable[Exception]): A callable used to determine if an
             exception raised by the target should be considered retryable.
