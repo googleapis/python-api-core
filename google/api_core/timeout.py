95cde20cdbf1c7269f1d06cd8554d01cdf6ceab7
Anthonios Partheniou <partheniou@google.com>
2025-01-12 13:30:06 +0000

    fix: resolve the issue where rpc timeout of 0 is used when timeout expires

--- google/api_core/timeout.py
+++ google/api_core/timeout.py
@@ -102,8 +102,7 @@ class TimeToDeadlineTimeout(object):
         def func_with_timeout(*args, **kwargs):
             """Wrapped function that adds timeout."""
 
-            remaining_timeout = self._timeout
-            if remaining_timeout is not None:
+            if self._timeout is not None:
                 # All calculations are in seconds
                 now_timestamp = self._clock().timestamp()
 
@@ -114,8 +113,20 @@ class TimeToDeadlineTimeout(object):
                     now_timestamp = first_attempt_timestamp
 
                 time_since_first_attempt = now_timestamp - first_attempt_timestamp
-                # Avoid setting negative timeout
-                kwargs["timeout"] = max(0, self._timeout - time_since_first_attempt)
+                remaining_timeout = self._timeout - time_since_first_attempt
+
+                # Although the `deadline` parameter in `google.api_core.retry.Retry`
+                # is deprecated, and should be treated the same as the `timeout`,
+                # it is still possible for `deadline` argument in google.api_core.retry.Retry
+                # to be larger than the `timeout`.
+                # Avoid setting negative timeout or timeout less than 5 seconds when the `timeout`
+                # has expired.
+                # See https://github.com/googleapis/python-api-core/issues/654
+                # Revert back to the original timeout when this happens
+                if remaining_timeout < 5:
+                    remaining_timeout = self._timeout
+
+                kwargs["timeout"] = remaining_timeout
 
             return func(*args, **kwargs)
 
